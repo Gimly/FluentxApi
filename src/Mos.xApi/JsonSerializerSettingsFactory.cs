374cb6e1542cc5c11bc2675123c482e280dc85b1
Xavier Hahn <xavier.hahn@gmail.com>
2017-02-16 17:03:52 +0100

    Added Xml documentation for JsonSerializerSettingsFactory

--- src/Mos.xApi/JsonSerializerSettingsFactory.cs
+++ src/Mos.xApi/JsonSerializerSettingsFactory.cs
@@ -1,14 +1,20 @@
 ﻿using Mos.xApi.Utilities;
 using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Mos.xApi
 {
+    /// <summary>
+    /// Helper factory class that creates the JsonSerializerSettings
+    /// configured to serialize and deserialize the library's objects
+    /// from/to Experience API JSON strings.
+    /// </summary>
     public static class JsonSerializerSettingsFactory
     {
+        /// <summary>
+        /// Returns the JsonSerializerSettings configured to parse and serialize
+        /// Experience API json strings.
+        /// </summary>
+        /// <returns>A correctly configured instance of JsonSerializerSettings.</returns>
         public static JsonSerializerSettings CreateSettings()
         {
             var settings = new JsonSerializerSettings();
