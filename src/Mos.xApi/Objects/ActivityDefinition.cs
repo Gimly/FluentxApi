8f923afd16b101f7a03f2daa71f325cc6f2fa608
Xavier Hahn <xavier.hahn@gmail.com>
2017-02-20 11:09:20 +0100

    Added Xml documentation for all Activity related classes.

--- src/Mos.xApi/Objects/ActivityDefinition.cs
+++ src/Mos.xApi/Objects/ActivityDefinition.cs
@@ -4,8 +4,19 @@ using System.Linq;
 
 namespace Mos.xApi.Objects
 {
+    /// <summary>
+    /// This class contains all properties used to define an Activity.
+    /// </summary>
     public class ActivityDefinition
     {
+        /// <summary>
+        /// Initializes a new instance of the ActivityDefinition class.
+        /// </summary>
+        /// <param name="name">The human readable/visual name of the Activity</param>
+        /// <param name="type">The type of Activity.</param>
+        /// <param name="description">A description of the Activity</param>
+        /// <param name="moreInfo">Resolves to a document with human-readable information about the Activity, which could include a way to launch the activity.</param>
+        /// <param name="extensions">A map of other properties as needed</param>
         public ActivityDefinition(ILanguageMap name, Uri type, ILanguageMap description = null, Uri moreInfo = null, Extension extensions = null)
         {
             if (name != null && name.Any())
@@ -28,18 +39,33 @@ namespace Mos.xApi.Objects
             
         }
 
+        /// <summary>
+        /// Gets a description of the Activity
+        /// </summary>
         [JsonProperty("description", Order = 1, NullValueHandling = NullValueHandling.Ignore)]
         public ILanguageMap Description { get; }
 
+        /// <summary>
+        /// Gets an IRI that resolves to a document with human-readable information about the Activity, which could include a way to launch the activity.
+        /// </summary>
         [JsonProperty("moreInfo", Order = 3, NullValueHandling = NullValueHandling.Ignore)]
         public Uri MoreInfo { get; }
 
+        /// <summary>
+        /// Gets the human readable/visual name of the Activity
+        /// </summary>
         [JsonProperty("name", Order = 0, NullValueHandling = NullValueHandling.Ignore)]
         public ILanguageMap Name { get; }
 
+        /// <summary>
+        /// Gets the type of Activity.
+        /// </summary>
         [JsonProperty("type", Order = 2, NullValueHandling = NullValueHandling.Ignore)]
         public Uri Type { get; }
 
+        /// <summary>
+        /// Gets a map of other properties as needed
+        /// </summary>
         [JsonProperty("extensions", Order = 4, NullValueHandling = NullValueHandling.Ignore)]
         public Extension Extensions { get; }
     }
